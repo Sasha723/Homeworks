26b77bb1f24073677fbd838fbba9ee77b2213cba
Sasha <kondrachuk55@gmail.com>
2020-02-14 00:56:41 +0200

    commited

---
+++ Phone Book/Phone Book/Class1.cs
@@ -0,0 +1,218 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Phone_Book
+{
+    public static class Class1
+    {
+        public static void Start()
+        {
+            LogManager.GetCurrentClassLogger().Info("User started the program.");
+            if (!Directory.Exists("Contacts"))
+            {
+                LogManager.GetCurrentClassLogger().Info("Directory \"Contacts\" has been created.");
+                Directory.CreateDirectory("Contacts");
+            }
+        }
+        public static string RenameContact(string oldName, string newName)
+        {
+            if (!File.Exists($@"Contacts\{newName}.txt"))
+            {
+                File.Move($@"Contacts\{oldName}.txt", $@"Contacts\{newName}.txt");
+                LogManager.GetCurrentClassLogger().Info($"User renamed contact {oldName} to contact {newName}.");
+                return $"Contact {oldName} has been renamed to contact {newName}.";
+            }
+            else
+            {
+                return $"Contact {newName} has been already exists.";
+            }
+        }
+        public static List<string> FindContact(string partOfName)
+        {
+            List<string> contacts = new List<string>();
+            foreach (var item in Directory.GetFiles("Contacts"))
+            {
+                var a = item.Substring(item.LastIndexOf('\\') + 1);
+                a = a.Substring(0, a.LastIndexOf('.'));
+                if (a.Contains(partOfName))
+                {
+                    contacts.Add(a);
+                }
+            }
+            LogManager.GetCurrentClassLogger().Info($"User found contact by part of name {partOfName}.");
+            return contacts;
+        }
+        public static string DeleteAllContacts(string code, string text)
+        {
+            if (code == text)
+            {
+                LogManager.GetCurrentClassLogger().Info("User deleted all contacts.");
+                Directory.Delete("Contacts", true);
+                Directory.CreateDirectory("Contacts");
+                return "All contacts have been deleted.";
+            }
+            else
+            {
+                return "You have inputed incorrect code.";
+            }
+        }
+        public static string DeleteContact(string name, string code, string text)
+        {
+            if (code == text)
+            {
+                LogManager.GetCurrentClassLogger().Info($"User deleted contact {name}.");
+                File.Delete($@"Contacts\{name}.txt");
+                return $"Contact {name} has been deleted.";
+            }
+            else
+            {
+                return "You have inputed incorrect code.";
+            }
+        }
+        public static string AddContact(string name)
+        {
+            if (!File.Exists($@"Contacts\{name}.txt"))
+            {
+                using (File.Create($@"Contacts\{name}.txt")) { }
+                string guid = Guid.NewGuid().ToString();
+                StreamWriter streamWriter = new StreamWriter($@"Contacts\{name}.txt");
+                streamWriter.WriteLine(guid + "¶");
+                streamWriter.Close();
+                LogManager.GetCurrentClassLogger().Info($"User added new contact {name}.");
+                return $"Contact {name} has been added.";
+            }
+            else
+            {
+                return $"Contact {name} has been already exists.";
+            }
+        }
+        public static List<string> ShowContacts()
+        {
+            List<string> contacts = new List<string>();
+            foreach (var item in Directory.GetFiles("Contacts"))
+            {
+                var a = item.Substring(item.LastIndexOf('\\') + 1);
+                a = a.Substring(0, a.LastIndexOf('.'));
+                contacts.Add(a);
+            }
+            LogManager.GetCurrentClassLogger().Info($"User showed all contacts.");
+            return contacts;
+        }
+        public static string WriteWay(string name, string wayToContact, string wayToContactText)
+        {
+            bool wayToContactInFile = false;
+            string[] fileText = File.ReadAllLines($@"Contacts\{name}.txt");
+            foreach (var item in fileText)
+            {
+                if (item.Contains($"{wayToContact}:"))
+                {
+                    wayToContactInFile = true;
+                    break;
+                }
+            }
+            LogManager.GetCurrentClassLogger().Info($"User added new way to contact {wayToContact} in contact {name}.");
+            if (wayToContactInFile == true)
+            {
+                string[] readText = File.ReadAllLines($@"Contacts\{name}.txt");
+                for (int i = 0; i < readText.Length; i++)
+                {
+                    if (readText[i].Contains(wayToContact))
+                    {
+                        readText[i] = $"{readText[i]}, {wayToContactText}";
+                    }
+                }
+                File.WriteAllLines($@"Contacts\{name}.txt", readText);
+                return "Text has been added.";
+            }
+            else
+            {
+                if (File.ReadAllLines($@"Contacts\{name}.txt").Length > 1)
+                {
+                    File.AppendAllText($@"Contacts\{name}.txt", $"\n{wayToContact}: {wayToContactText}");
+                }
+                else
+                {
+                    File.AppendAllText($@"Contacts\{name}.txt", $"{wayToContact}: {wayToContactText}");
+                }
+                return $"Way to contact {wayToContact} has been added.";
+            }
+        }
+        public static List<string> ShowWays(string name)
+        {
+            StreamReader streamReader = new StreamReader($@"Contacts\{name}.txt");
+            string line = streamReader.ReadLine();
+            List<string> lines = new List<string>();
+            line = streamReader.ReadLine();
+            while (line != null)
+            {
+                lines.Add(line);
+                line = streamReader.ReadLine();
+            }
+            streamReader.Close();
+            LogManager.GetCurrentClassLogger().Info($"User showed all ways to contact in contact {name}.");
+            return lines;
+        }
+        public static string DeleteWay(string name, string wayToContact)
+        {
+            string[] text = File.ReadAllLines($@"Contacts\{name}.txt");
+            bool wayExists = false;
+            foreach (var item in text)
+            {
+                if (item.Contains($"{wayToContact}:"))
+                {
+                    LogManager.GetCurrentClassLogger().Info($"User deleted way to contact {wayToContact} in contact {name}.");
+                    wayExists = true;
+                    List<string> readText = new List<string>(File.ReadAllLines($@"Contacts\{name}.txt"));
+                    for (int i = 0; i < readText.Count; i++)
+                    {
+                        if (readText[i].Contains(wayToContact))
+                        {
+                            readText.Remove(readText[i]);
+                        }
+                    }
+                    File.WriteAllLines($@"Contacts\{name}.txt", readText);
+                }
+            }
+            if (wayExists == false)
+            {
+                return $"Way to contact {wayToContact} does not exists.";
+            }
+            else
+            {
+                return $"Way to contact {wayToContact} has been deleted.";
+            }
+        }
+        public static string RenameWay(string name, string oldWayToContact, string newWayToContact)
+        {
+            string[] text = File.ReadAllLines($@"Contacts\{name}.txt");
+            bool newWayToContactExists = false;
+            foreach (var item in text)
+            {
+                if (item.Contains($"{newWayToContact}: "))
+                {
+                    newWayToContactExists = true;
+                }
+            }
+            if (newWayToContactExists == false)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i].Contains($"{oldWayToContact}: "))
+                    {
+                        var a = text[i].Substring(oldWayToContact.Length + 1);
+                        text[i] = $"{newWayToContact}:{a}";
+                    }
+                }
+                File.WriteAllLines($@"Contacts\{name}.txt", text);
+                LogManager.GetCurrentClassLogger().Info($"User renamed way to contact {oldWayToContact} to way to contact {newWayToContact} in contact {name}.");
+                return $"Way to contact {oldWayToContact} has been renamed to way to contact {newWayToContact}.";
+            }
+            else
+            {
+                return $"Way to contact {newWayToContact} has been already exists.";
+            }
+        }
+    }
+}
---
+++ Phone Book/PhoneBook_Console/Program.cs
@@ -0,0 +1,352 @@
+﻿using NLog;
+using Phone_Book;
+using System;
+using System.IO;
+
+namespace PhoneBook_Console
+{
+    class Program
+    {
+        static bool exit = false;
+        static bool allContactsDelete = false;
+        static void Main(string[] args)
+        {
+            Class1.Start();
+        start:
+            Console.Clear();
+            if (Directory.GetFiles("Contacts").Length > 0)
+            {
+                string answer = Action();
+                if (answer != "All contacts has been deleted.")
+                {
+                    Console.Clear();
+                    Console.WriteLine(answer);
+                }
+                else
+                {
+                    goto start;
+                }
+            }
+            else
+            {
+                Console.Write("You don't have any contacts.\nTo add contact input contact's name: ");
+                string name = Console.ReadLine();
+                Console.Clear();
+                try
+                {
+                    Console.WriteLine(Class1.AddContact(name));
+                    allContactsDelete = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You inputed incorrect name.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto start;
+                }
+                Console.ReadKey();
+                Action();
+                goto start;
+            }
+            LogManager.GetCurrentClassLogger().Info($"User finished the program.");
+        }
+        static string Action()
+        {
+        action:
+            if (exit == true)
+            {
+                return "Thank you for using my telephone book :-)";
+            }
+            if (allContactsDelete == true)
+            {
+                return "All contacts has been deleted.";
+            }
+            Console.Clear();
+            Console.WriteLine("What do you want to do?\n1 — Add a contact;\n2 — Edit a contact or show contact's ways to contact;\n3 — Show all contacts;\n4 — Delete a contact;\n5 — Delete all contacts;\n6 — Find a contact by part of contact's name;\n7 — Rename a contact;\nTo exit input \"exit\".");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    {
+                        Console.Clear();
+                        Console.Write("Contact's name: ");
+                        string name = Console.ReadLine();
+                        Console.Clear();
+                        try
+                        {
+                            Console.WriteLine(Class1.AddContact(name));
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("You inputed incorrect name.");
+                        }
+                        Console.ReadKey();
+                    }
+                    break;
+                case "2":
+                    {
+                        Console.Clear();
+                        Console.Write("Contact's name: ");
+                        string name = Console.ReadLine();
+                        Console.Clear();
+                        if (File.Exists($@"Contacts\{name}.txt"))
+                        {
+                            if (File.ReadAllLines($@"Contacts\{name}.txt").Length > 1)
+                            {
+                                Edit(name);
+                            }
+                            else
+                            {
+                            edit:
+                                Console.Write($"You don't have any ways to contact in contact {name}.\nTo add way to contact input way's to contact name: ");
+                                string wayToContact = Console.ReadLine();
+                                Console.Clear();
+                                if (wayToContact != "")
+                                {
+                                    Console.Write("Text: ");
+                                    string text = Console.ReadLine();
+                                    Console.Clear();
+                                    Console.WriteLine(Class1.WriteWay(name, wayToContact, text));
+                                    Console.ReadKey();
+                                    Edit(name);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("You inputed incorrect name.");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    goto edit;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Contact {name} does not exists.");
+                        }
+                    }
+                    break;
+                case "3":
+                    {
+                        Console.Clear();
+                        int number = 1;
+                        foreach (var item in Class1.ShowContacts())
+                        {
+                            Console.WriteLine(number + " — " + item);
+                            number++;
+                        }
+                        Console.ReadKey();
+                    }
+                    break;
+                case "4":
+                    {
+                        Console.Clear();
+                        string code = new Random().Next(1000, 10000).ToString();
+                        Console.Write("Contact's name: ");
+                        string name = Console.ReadLine();
+                        Console.Clear();
+                        if (File.Exists($@"Contacts\{name}.txt"))
+                        {
+                            Console.Write($"Enter {code} to delete contact {name}: ");
+                            string text = Console.ReadLine();
+                            Console.Clear();
+                            string answer = Class1.DeleteContact(name, code, text);
+                            if (Directory.GetFiles("Contacts").Length == 0)
+                            {
+                                allContactsDelete = true;
+                                return "All contacts have been deleted.";
+                            }
+                            else
+                            {
+                                Console.WriteLine(answer);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Contact {name} does not exists.");
+                        }
+                        Console.ReadKey();
+                    }
+                    break;
+                case "5":
+                    {
+                        Console.Clear();
+                        string code = new Random().Next(1000, 10000).ToString();
+                        Console.Write($"Enter {code} to delete all contacts: ");
+                        string text = Console.ReadLine();
+                        Console.Clear();
+                        string answer = Class1.DeleteAllContacts(code, text);
+                        if (answer == "All contacts have been deleted.")
+                        {
+                            Console.WriteLine(answer);
+                            Console.ReadKey();
+                            allContactsDelete = true;
+                            return answer;
+                        }
+                        else
+                        {
+                            Console.WriteLine(answer);
+                        }
+                    }
+                    break;
+                case "6":
+                    {
+                        Console.Clear();
+                        int number = 1;
+                        Console.Write("Part of contact's name: ");
+                        string partOfName = Console.ReadLine();
+                        Console.Clear();
+                        if (Class1.FindContact(partOfName).Count > 0)
+                        {
+                            foreach (var item in Class1.FindContact(partOfName))
+                            {
+                                Console.WriteLine(number + " — " + item);
+                                number++;
+                            }
+                            Console.Write("Contact's name that you want to edit or view: ");
+                            Edit(Console.ReadLine());
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Contact with part of name {partOfName} does not exists.");
+                            Console.ReadKey();
+                        }
+                    }
+                    break;
+                case "7":
+                    {
+                        Console.Clear();
+                        Console.Write("Contacts name: ");
+                        string oldName = Console.ReadLine();
+                        Console.Clear();
+                        if (File.Exists($@"Contacts\{oldName}.txt"))
+                        {
+                            Console.Write("New contact's name: ");
+                            string newName = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine(Class1.RenameContact(oldName, newName));
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Contact {oldName} does not exists.");
+                        }
+                    }
+                    break;
+                case "exit":
+                    exit = true;
+                    return "Thank you for using my telephone book :-)";
+                default:
+                    Console.Clear();
+                    Console.WriteLine("You inputed incorrect value.");
+                    Console.ReadKey();
+                    break;
+            }
+            goto action;
+        }
+
+        private static string Edit(string name)
+        {
+        edit:
+            Console.Clear();
+            Console.WriteLine($"What do you want to do with contact {name}?\n1 — Add a way to contact;\n2 — Show all ways to contact;\n3 — Delete a way to contact;\n4 — Rename a way to contact;\nTo go to menu input \"menu\".");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    {
+                        Console.Clear();
+                        Console.Write("Way to contact: ");
+                        string wayToContact = Console.ReadLine();
+                        Console.Clear();
+                        if (wayToContact != "")
+                        {
+                            Console.Write("Text: ");
+                            string text = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine(Class1.WriteWay(name, wayToContact, text));
+                        }
+                        else
+                        {
+                            Console.WriteLine("You inputed incorrect name.");
+                        }
+                        Console.ReadKey();
+                    }
+                    break;
+                case "2":
+                    Console.Clear();
+                    int number = 1;
+                    foreach (var item in Class1.ShowWays(name))
+                    {
+                        Console.WriteLine(number + " — " + item);
+                        number++;
+                    }
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    {
+                    delete:
+                        Console.Clear();
+                        Console.Write("Way to contact: ");
+                        string wayToContact = Console.ReadLine();
+                        if (wayToContact == "")
+                        {
+                            goto delete;
+                        }
+                        Console.Clear();
+                        Console.WriteLine(Class1.DeleteWay(name, wayToContact));
+                        Console.ReadKey();
+                        if (File.ReadAllLines($@"Contacts\{name}.txt").Length > 1)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.Write($"You don't have any ways to contact in contact {name}.\nTo add way to contact input way's to contact name: ");
+                            string wayToContact2 = Console.ReadLine();
+                            Console.Clear();
+                            Console.Write("Now you have to input your way's to contact text: ");
+                            Class1.WriteWay(name, wayToContact2, Console.ReadLine());
+                        }
+                    }
+                    break;
+                case "4":
+                    {
+                        bool oldWayToContactExists = false;
+                        Console.Clear();
+                        Console.Write("Way to contact: ");
+                        string oldWayToContact = Console.ReadLine();
+                        Console.Clear();
+                        foreach (var item in File.ReadAllLines($@"Contacts\{name}.txt"))
+                        {
+                            if (item.Contains($"{oldWayToContact}:"))
+                            {
+                                oldWayToContactExists = true;
+                            }
+                        }
+                        if (oldWayToContactExists == true)
+                        {
+                            Console.Write("New name of way to contact: ");
+                            string newWayToContact = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine(Class1.RenameWay(name, oldWayToContact, newWayToContact));
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Way to contact {oldWayToContact} does not exists.");
+                        }
+                        Console.ReadKey();
+                    }
+                    break;
+                case "menu":
+                    Console.Clear();
+                    Action();
+                    return "";
+                default:
+                    Console.Clear();
+                    Console.WriteLine("You inputed incorrect value.");
+                    Console.ReadKey();
+                    break;
+            }
+            goto edit;
+        }
+    }
+}
